1121a7c4fd4787872953595d16e307c7b7a229d3
Elias <eduarte@chicagotechacademy.org>
2015-09-16 21:00:54 -0500

    Create Assignment3.rb

---
+++ Assignment3.rb
@@ -0,0 +1,7 @@
+Puts "how many hours do you work?"
+Hours = gets.to_i
+Puts "how much do you make per hour?"
+Money = gets.to_i
+Puts "how many weeks did you work?"
+Weeks = gets.to_i
+Salary = Hours * Money * Weeks
